fb92ec689baba3c1ce9433ec39570d42dcbddd2c
SnehilDwivedi <snehildwivedi13@gmail.com>
2025-04-11 17:55:03 +0530

    Your commit message

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,12 @@
 const app = require('./app');
+
 const PORT = process.env.PORT || 5000;
+
+
+
+app.get('/health', (req, res) => {
+  res.sendStatus(200);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
